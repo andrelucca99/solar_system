5504bcabb39100460d6a5ebd8c00e93a5ea49044
André Lucas <andrelucca99@gmail.com>
2025-10-05 08:14:37 -0300

    feat: add component header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,8 @@
 export function Header() {
-  return <h1 className="text-3xl text-blue-500">Sistema Solar</h1>
+  return (
+    <header className="mb-5">
+      <h1 className="text-6xl font-bold text-white">Sistema Solar</h1>
+      <h2 className="text-sm mt-2 font-normal text-blue-500">EXPLORANDO NOSSA VIZINHAÇA CÓSMICA</h2>
+    </header>
+  );
 };
