5a8f040aed4442f248767bb20bc87b5f3d7044b3
André Lucas <andrelucca99@gmail.com>
2025-10-08 12:20:05 -0300

    style: add imagm de fundo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Footer } from "./components/Footer/Footer"
 import { Header } from "./components/Header/Header"
 import { SolarSystem } from "./components/SolarSystem/SolarSystem"
+import bgImage from "./images/background.png"
 
 function App() {
 
   return (
-    <main className="bg-black gap-5 text-white flex flex-col text-center p-5">
+    <main
+      className="bg-no-repeat bg-cover min-h-screen gap-5 text-white flex flex-col text-center p-5"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
       <Header />
       <SolarSystem />
       <Footer />
